f15f7301a863e496576766bbb432f6ed501995de
Andrew Kim <commanderjacks@gmail.com>
2019-03-12 19:55:41 -0700

    Simple demo was added for class

---
+++ demo.js
@@ -0,0 +1,46 @@
+
+
+var firstName = "Bob";
+console.log(firstName);
+console.log(5 + 4);
+
+function myFunction(p1, p2) {
+	return p1 * p2;
+}
+
+// console.log(myFunction(2, 3));
+// console.log(myFunction("Alex", 3));
+// console.log(myFunction("Alex", "Bob"));
+// console.log(myFunction("2", "3"));
+
+// var x = myFunction(4, 3);
+// console.log(x);
+
+
+// function concaten(x, y) {
+// 	return x + y;
+// }
+
+// // concatenating with local function
+// console.log(concaten(2, 3));
+// console.log(concaten("Alex", 3));
+// console.log(concaten("Alex", "Bob"));
+// console.log(concaten("4", "10"));
+
+// // accessing arrays
+// var males = ["Alex", "Cole", "Derek"];
+// var femes = ["Alex", "Jesse", "Lae"];
+// console.log(males[0]);
+// console.log(femes[1]);
+
+// // weird way to add arrays
+// var nameRes = males + femes;
+// console.log(nameRes);
+// console.log(nameRes[0]);
+// console.log(nameRes[1]);
+
+// // actual way to add arrays
+// var list = males.concat(femes);
+// console.log(list);
+// console.log(list[2]);
+
